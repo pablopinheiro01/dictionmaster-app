0bcbc47171d0f930e1d52d01847e157c21cce61d
pablopinheiro01 <pablo-pinheiro@hotmail.com>
2024-01-22 17:41:19 -0300

    created view model to manipulate datas and show states in the view

--- app/src/main/java/br/com/dictionmaster/ui/viewmodels/SearchViewModel.kt
+++ app/src/main/java/br/com/dictionmaster/ui/viewmodels/SearchViewModel.kt
@@ -1,82 +1,46 @@
 package br.com.dictionmaster.ui.viewmodels
 
-import androidx.compose.ui.text.toLowerCase
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import br.com.dictionmaster.repositories.SearchRepository
 import br.com.dictionmaster.ui.uistates.SearchUiState
-import br.com.dictionmaster.util.firstCharToUpperCase
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
-import kotlinx.coroutines.launch
-import java.util.Locale
 import javax.inject.Inject
 
 @HiltViewModel
-class SearchViewModel @Inject constructor(
-    private val repository: SearchRepository
-) : ViewModel() {
-    private val _uiState = MutableStateFlow(SearchUiState())
+class SearchViewModel @Inject constructor() : ViewModel() {
+
+    private val _wordMutableStateFlow = MutableStateFlow("")
+
+    val wordMutableStateFlow: StateFlow<String>
+        get() = _wordMutableStateFlow
+
+    private val _uiState = MutableStateFlow<SearchUiState>(
+        SearchUiState.SuccessSearchUiState()
+    )
 
     val uiState: StateFlow<SearchUiState>
         get() = _uiState.asStateFlow()
 
-    init {
-        viewModelScope.launch(Dispatchers.IO) {
-            _uiState.update { currentState ->
-                currentState.copy(
-                    onValueChangedSearchWord = { word ->
-                        _uiState.value = _uiState.value.copy(
-                            word = word,
-                            showError = false
-                        )
-                    },
-                    onSearchButtonClick = {
-                        search()
-                    }
-                )
-            }
-        }
-    }
-
-    fun search() {
-        viewModelScope.launch(Dispatchers.IO) {
-            _uiState.update { currentState ->
-                currentState.copy(
-                    onLoading = true,
-                )
-            }
-            try{
-                val searchResult = repository.search(_uiState.value.word.firstCharToUpperCase())
-                if (searchResult.isNullOrEmpty()) {
-                    _uiState.update { currentState ->
-                        currentState.copy(
-                            showError = true,
-                            onLoading = false
-                        )
-                    }
-                } else {
-                    _uiState.update { currentState ->
-                        currentState.copy(
-                            showError = false,
-                            onLoading = false,
-                            onNavigateToShowResults = true
-                        )
-                    }
-                }
-            }catch(e: Exception){
-                _uiState.update { currentState ->
-                    currentState.copy(
-                        onNavigateBuyApp = true
+    fun onValueChangedSearchWord(word: String) {
+        _wordMutableStateFlow.value = word
+        _uiState.value = _uiState.value.let {
+            when (it) {
+                is SearchUiState.SuccessSearchUiState -> {
+                    SearchUiState.SuccessSearchUiState(
+                        word = word,
+                        onValueChangedSearchWord = { onValueChangedSearchWord(it) }
                     )
                 }
+                else -> {}
             }
-        }
+        } as SearchUiState
     }
 
+    fun changed() {
+        _uiState.update { SearchUiState.SuccessSearchUiState() }
+    }
 
 }
