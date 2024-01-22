33508f6f36aeb7b68d9c7eef47723992a5bee3e3
pablopinheiro01 <pablo-pinheiro@hotmail.com>
2024-01-22 17:39:48 -0300

    configured business role domain layer.

--- app/src/main/java/br/com/dictionmaster/repositories/SearchRepository.kt
+++ app/src/main/java/br/com/dictionmaster/repositories/SearchRepository.kt
@@ -1,18 +1,21 @@
 package br.com.dictionmaster.repositories
 
-import android.util.Log
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
 import br.com.dictionmaster.database.dao.WordsDao
 import br.com.dictionmaster.database.entities.WordDetailEntity
+import br.com.dictionmaster.exceptions.AllAttemptsMadeBuyAppException
+import br.com.dictionmaster.exceptions.DataNotFoundException
 import br.com.dictionmaster.model.WordDetail
 import br.com.dictionmaster.model.toWordDetail
+import br.com.dictionmaster.network.data.WordDetailResponse
 import br.com.dictionmaster.network.services.DictionMasterService
 import br.com.dictionmaster.preferences.PreferencesKey
-import br.com.dictionmaster.util.convertFromJsonGeneric
+import br.com.dictionmaster.util.convertFromJsonGenericList
 import br.com.dictionmaster.util.convertToJsonGeneric
 import kotlinx.coroutines.flow.first
+import retrofit2.Response
 import javax.inject.Inject
 
 
@@ -22,82 +25,110 @@ class SearchRepository @Inject constructor(
     private val dataStore: DataStore<Preferences>
 ) {
 
-    suspend fun search(word: String): List<WordDetail?> {
+    suspend fun search(word: String): List<WordDetail> {
 
         if(calculateSearchs()){
-            throw Exception()
+            throw AllAttemptsMadeBuyAppException()
         }
 
-        return try {
+        val result: List<WordDetail> = searchDao(word)
 
-            val resultDao = searchDao(word)
-
-            if (resultDao.isNullOrEmpty()) {
-                searchApi(word)
-            }
-            resultDao
-
-        } catch (e: Exception) {
-            e.printStackTrace()
-            Log.i("SearchRepository", "error $e")
-            emptyList()
-        }
-
-
-    }
-
-    private suspend fun searchDao(word: String): List<WordDetail?> {
-        val list = mutableListOf<WordDetail?>()
-
-        dao.findWord(word).let { listWord ->
-            listWord.forEach { word ->
-                val converted = convertFromJsonGeneric<WordDetail>(word.json)
-                list.add(converted)
+        result.let {
+            if (it.isNullOrEmpty()) {
+                return searchApi(word)
             }
         }
-
-        return list.toList()
+        return result
     }
 
-    private suspend fun searchApi(word: String): List<WordDetail?> {
-        service.search(word).let { response ->
-            Log.i("SearchRepository", "search: ${response.body()}")
+    private fun searchDao(word: String): List<WordDetail> {
+        val listReturned = mutableListOf<WordDetail>()
 
+        dao.findWord(word)?.map { listWordDetailEntity ->
+            transformFromJson(listWordDetailEntity, listReturned)
+        }
+        return listReturned.toList()
+    }
+
+    private fun transformFromJson(
+        listWordDetailEntity: WordDetailEntity,
+        listReturned: MutableList<WordDetail>
+    ): MutableList<WordDetail> {
+        listWordDetailEntity?.let { detailEntity ->
+            val itemConverted = convertFromJsonGenericList<WordDetail>(detailEntity.json)
+            listReturned.addAll(itemConverted ?: emptyList())
+        }
+        return listReturned
+    }
+
+    private suspend fun searchApi(word: String): List<WordDetail> {
+        return service.search(word).let { response ->
             when {
                 response.isSuccessful -> {
-                    response.body()?.map { listWordDetailResponse ->
-                        val json = convertToJsonGeneric(listWordDetailResponse)
-                        json?.let {
-                            dao.save(WordDetailEntity(word = word, json = json))
-                        }
-                    }
-                    return response.body()?.map { wordDetailResponse ->
-                        wordDetailResponse.toWordDetail()
-                    } ?: run {
-                        emptyList()
-                    }
+                    dataTreatment(response, word)
                 }
-
                 else -> {
-                    return emptyList()
+                    throw DataNotFoundException()
                 }
             }
 
         }
     }
 
-    private suspend fun calculateSearchs(): Boolean{
-        dataStore.edit {
-            it[PreferencesKey.USER_QUANTITY_SEARCHS] = 1
-//            it[PreferencesKey.USER_QUANTITY_SEARCHS] = 1+(dataStore.data.first()[PreferencesKey.USER_QUANTITY_SEARCHS]?:0)
+    private fun dataTreatment(
+        response: Response<List<WordDetailResponse>>,
+        word: String
+    ) = response.body()?.let { listWordDetailResponse ->
+        convertToJson(listWordDetailResponse, word)
+        transform(listWordDetailResponse)
+    } ?: run {
+        emptyList()
+    }
+
+    private fun transform(listWordDetailResponse: List<WordDetailResponse>) =
+        listWordDetailResponse.map { wordDetailResponse ->
+            wordDetailResponse.toWordDetail()
         }
-        dataStore.data.first()[PreferencesKey.USER_QUANTITY_SEARCHS]?.let{
-            Log.i("SearchRepository", "calculateSearchs: $it")
-            if(it > 10){
+
+    private fun convertToJson(
+        listWordDetailResponse: List<WordDetailResponse>,
+        word: String
+    ) {
+        val json = convertToJsonGeneric(listWordDetailResponse)
+        json?.let {
+            dao.save(WordDetailEntity(word = word, json = json))
+        }
+    }
+
+    suspend fun calculateSearchs(): Boolean {
+        plusValue()
+        return verifyIfUserCanContinueUseApp()
+    }
+
+    private suspend fun verifyIfUserCanContinueUseApp(): Boolean {
+        dataStore.data.first()[PreferencesKey.USER_QUANTITY_SEARCHS]?.let {
+            if (it > 10) {
                 return true
             }
         }
-        return false
+            return false
+    }
+
+    private suspend fun plusValue() {
+        dataStore.edit {
+            it[PreferencesKey.USER_QUANTITY_SEARCHS] =
+                1 + (dataStore.data.first()[PreferencesKey.USER_QUANTITY_SEARCHS] ?: 0)
+        }
+    }
+
+    suspend fun clean() {
+        dataStore.edit {
+            it[PreferencesKey.USER_QUANTITY_SEARCHS] = 0
+        }
+    }
+
+    companion object {
+        const val TAG = "SearchRepository"
     }
 }
 
