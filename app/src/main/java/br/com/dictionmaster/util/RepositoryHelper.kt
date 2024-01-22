5a73ba3e7d2dcc571bec42d1f1a4ae967b915256
pablopinheiro01 <pablo-pinheiro@hotmail.com>
2024-01-22 17:41:44 -0300

    created helper objects to manipulate json

--- app/src/main/java/br/com/dictionmaster/util/RepositoryHelper.kt
+++ app/src/main/java/br/com/dictionmaster/util/RepositoryHelper.kt
@@ -5,11 +5,18 @@ import com.squareup.moshi.Moshi
 import com.squareup.moshi.Types
 import java.lang.reflect.Type
 
-inline fun <reified T> convertToJsonGeneric(obj: T): String? {
+inline fun <reified T> convertToJsonGeneric(obj: List<T>): String? {
     val moshi = Moshi.Builder().build()
-    val type: Type = Types.newParameterizedType(T::class.java)
-    val adapter: JsonAdapter<T> = moshi.adapter(type)
+    val type: Type = Types.newParameterizedType(List::class.java, T::class.java)
+    val adapter: JsonAdapter<List<T>> = moshi.adapter(type)
     return adapter.toJson(obj)
+}
+
+inline fun <reified T> convertFromJsonGenericList(value: String): List<T>? {
+    val moshi = Moshi.Builder().build()
+    val type: Type = Types.newParameterizedType(List::class.java, T::class.java)
+    val adapter: JsonAdapter<List<T>> = moshi.adapter(type)
+    return adapter.fromJson(value)
 }
 
 inline fun <reified T> convertFromJsonGeneric(value: String): T? {
